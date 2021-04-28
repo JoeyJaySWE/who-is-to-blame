e8e7616082ad305b94b96b0173523991e25299cd
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-04-28 15:10:30 +0200

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import Phaser from 'phaser';
+import Game from './scenes/game'
+
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: 1280,
+    height: 780,
+    scene: [
+        Game
+    ]
+};
+
+// tested
+
+
+
+const game = new Phaser.Game(config);
