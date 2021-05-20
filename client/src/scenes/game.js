3164e7af53830f51688202c378b60e7ce543ddaa
JoeyJaySWE <joeyjay@joeyjaydigital.com>
2021-05-20 13:26:53 +0200

    Added comment markers
    Made it easier to read the code by adding comments detailing what each part does.
    
    Can also be used as anchors using the Ctrl/cmd + F function in the editor.

--- client/src/scenes/game.js
+++ client/src/scenes/game.js
@@ -21,30 +21,48 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    // let hostUs÷er;
+    // ----- [ Init Variabels ] --------
+
+    //fix for scoping access
     let gameScene = this;
+    // the current player who's view we're watching
     this.player = 'Default';
+    // all players
     let players;
+    // which players turn is it
     this.onStand;
+    // which card does the player have on hand
     this.playerHand = {
       evdenceCards: [],
       blameCards: [],
     };
+    // who's the Judge
     this.hostName;
+
+    // ----------------------------------
+
+    // --------- [ place we drop our evidnece cards ] ---------------------
 
     this.evidenceZone = new Zone(this, 300, 200, 240, 360, 'evidence');
     this.evidenceDropZone = this.evidenceZone.renderZone();
     this.outline = this.evidenceZone.renderOutline(this.evidenceDropZone);
 
+    // ---------------------------------------------------------------------
+
+    // --------- [ place we drop our evidnece cards ] ---------------------
+
     this.blameZone = new Zone(this, 600, 200, 240, 360, 'blame');
     this.blameDropZone = this.blameZone.renderZone();
     this.outline = this.blameZone.renderOutline(this.blameDropZone);
+
+    // --------------------------------------------------------------------
+
+    //socket set up
     this.socket = io.connect('http://localhost:3000');
     // let gameSetup = new GameSetUp(this, this.socket);
     this.socket.on('userJoined', (arg) => {
       console.log(`User Joined: ${arg}`);
     });
-    // const socket = socketIOClient.connect(SERVER_URL);
 
     // IF PLAYER JOIN
     // 1. check if player ain't  host
@@ -80,27 +98,35 @@ export default class Game extends Phaser.Scene {
       players = JSON.parse(arg);
       // gameScene.GameSetUp.buildLobby(players);
     });
+
+    // first setup
     this.socket.on('playerId', (arg) => {
       console.log(`this is playerId ${arg}`);
       gameScene.player = arg;
       gameScene.onStand = 'user2';
 
+      // displayes the players name in lower left corner
       gameScene.playerLabel = gameScene.add
         .text(75, 700, [arg])
         .setFont('Tithilum Web', 'Sans-serif')
         .setFontSize(24)
         .setColor('#0de');
 
+      // Disaplays whos turn it is in the center top of screen
       gameScene.turnIndicator = gameScene.add
         .text(400, 50, [`${gameScene.onStand} got the stand`])
         .setFont('Tithilum Web', 'Sans-serif')
         .setFontSize(42)
         .setColor('#f50');
 
+      //if current user ain't judge, show player view
       if (arg !== 'user1') {
         playGame(arg, 'user2');
       }
+
+      //if current user is judge, shpw judge view
       if (arg === 'user1') {
+        //adds a text to inidicate the option to choose player to take stand
         gameScene.indicatorLabel = gameScene.add
           .text(400, 100, [`Who shall take the stand?`])
           .setFont('Tithilum Web', 'Sans-serif')
@@ -118,21 +144,40 @@ export default class Game extends Phaser.Scene {
         if (this.playerHand.evdenceCards[i] != null) {
           continue;
         }
-        let playerCard = new Card(this, 0.05, 'evidence');
 
-        this.playerHand.evdenceCards[i] = playerCard.render(
-          300 + i * 100,
-          670,
-          'backside'
-        );
-        gameScene.playerHand.evdenceCards[i].on('pointerover', function () {
-          this.scale = 0.075;
-          gameScene.children.bringToTop(this);
-        });
+        if (i === 1) {
+          let playerCard = new Card(this, 0.1, 'evidence');
+          this.playerHand.evdenceCards[i] = playerCard.render(
+            300 + i * 100,
+            670,
+            'evidence'
+          );
 
-        gameScene.playerHand.evdenceCards[i].on('pointerout', function () {
-          this.scale = 0.05;
-        });
+          gameScene.playerHand.evdenceCards[i].on('pointerover', function () {
+            this.scale = 0.2;
+            gameScene.children.bringToTop(this);
+          });
+
+          gameScene.playerHand.evdenceCards[i].on('pointerout', function () {
+            this.scale = 0.1;
+          });
+        } else {
+          let playerCard = new Card(this, 0.05, 'evidence');
+          this.playerHand.evdenceCards[i] = playerCard.render(
+            300 + i * 100,
+            670,
+            'backside'
+          );
+
+          gameScene.playerHand.evdenceCards[i].on('pointerover', function () {
+            this.scale = 0.075;
+            gameScene.children.bringToTop(this);
+          });
+
+          gameScene.playerHand.evdenceCards[i].on('pointerout', function () {
+            this.scale = 0.05;
+          });
+        }
       }
 
       for (let i = 0; i < 3; i++) {
@@ -160,6 +205,7 @@ export default class Game extends Phaser.Scene {
     };
 
     function judgeGame() {
+      // ads text/button for making it player2's turn
       gameScene.switchToPlayer2 = gameScene.add
         .text(400, 140, [`Player 2`])
         .setFont('Tithilum Web', 'Sans-serif')
@@ -167,6 +213,7 @@ export default class Game extends Phaser.Scene {
         .setColor('#009900')
         .setInteractive();
 
+      // ads text/button for making it player3's turn
       gameScene.switchToPlayer3 = gameScene.add
         .text(650, 140, [`Player 3`])
         .setFont('Tithilum Web', 'Sans-serif')
@@ -174,24 +221,29 @@ export default class Game extends Phaser.Scene {
         .setColor('#660066')
         .setInteractive();
 
+      // sets hover state player 2
       gameScene.switchToPlayer2.on('pointerover', () => {
         gameScene.switchToPlayer2.setColor('#00DD00');
       });
 
+      // reset text color post-hover state player 2
       gameScene.switchToPlayer2.on('pointerout', () => {
         gameScene.switchToPlayer2.setColor('#009900');
       });
 
+      // makes it player 2's turn
       gameScene.switchToPlayer2.on('pointerdown', () => {
         gameScene.switchToPlayer2.setColor('#009900');
         gameScene.switchToPlayer3.setColor('#660066');
         gameScene.socket.emit('SwitchTurn', 'user2');
       });
 
+      // sets hover state player 3
       gameScene.switchToPlayer3.on('pointerover', () => {
         gameScene.switchToPlayer3.setColor('#DD00DD');
       });
 
+      // reset text color post-hover state player 3
       gameScene.switchToPlayer3.on('pointerdown', () => {
         gameScene.switchToPlayer2.setColor('#006600');
         gameScene.switchToPlayer3.setColor('#990099');
@@ -199,12 +251,15 @@ export default class Game extends Phaser.Scene {
         gameScene.socket.emit('SwitchTurn', 'user3');
       });
 
+      // makes it player 3's turn
       gameScene.switchToPlayer3.on('pointerout', () => {
         gameScene.switchToPlayer3.setColor('#660066');
       });
     }
 
+    // Player views
     function playGame(playerId, onStand) {
+      // Adds a text/button for drawing cards
       gameScene.dealText = gameScene.add
         .text(75, 350, ['Draw Card'])
         .setFont('Tithilum Web', 'Sans-serif')
@@ -212,11 +267,14 @@ export default class Game extends Phaser.Scene {
         .setColor('#4a0')
         .setInteractive();
 
+      // a player can only draw cards if it's that players turn
       gameScene.dealText.on('pointerdown', function () {
         if (playerId === onStand) {
           gameScene.dealCard();
         }
       });
+
+      // ------------[ Hover functions of the "draw card" text ]-------------------
 
       gameScene.dealText.on('pointerover', function () {
         gameScene.dealText.setColor('#470');
@@ -226,8 +284,15 @@ export default class Game extends Phaser.Scene {
         gameScene.dealText.setColor('#4a0');
       });
 
+      // --------------------------------------------------------------------------
+
+      // ------------[ move functions for our cards ]-------------------
+
+      //while we move
       gameScene.input.on('dragstart', function (pointer, gameObject) {
         gameObject.setTint(0xff69ba);
+
+        //scales card based on type (had old cards that had different sizes)
         if (gameObject.data.list.cardType === 'blame') {
           gameObject.scale = 0.19;
         } else {
@@ -236,8 +301,11 @@ export default class Game extends Phaser.Scene {
         gameScene.children.bringToTop(gameObject);
       });
 
+      // when we let go of the card
       gameScene.input.on('dragend', function (pointer, gameObject, dropped) {
         gameObject.setTint();
+
+        //if card wasn't dropped in a zone, send it back to the hand
         if (!dropped) {
           if (gameObject.data.list.cardType === 'blame') {
             gameObject.scale = 0.1;
@@ -249,33 +317,46 @@ export default class Game extends Phaser.Scene {
         }
       });
 
+      // gets our cards current position
       gameScene.input.on('drag', function (pointer, gameObject, dragX, dragY) {
         gameObject.x = dragX;
         gameObject.y = dragY;
       });
 
-      if (gameScene.playerHand.evdenceCards[1]) {
-      }
+      // -------------------------------------------------------------------------------
+
+      // When we dropp the card in a pile
       gameScene.input.on(
         'drop',
         function (pointer, gameObject, evidenceDropZone) {
+          //if we dropped in the Evidence pile
           if (gameObject.x > 300 && gameObject.x < 540) {
+            // if we dropped a Blame card in the evidence pile, send it back to hand
             if (gameObject.data.list.cardType !== 'evidence') {
               gameObject.scale = 0.1;
               gameObject.x = gameObject.input.dragStartX;
               gameObject.y = gameObject.input.dragStartY;
               return;
             }
+
+            // if it was dropped in the right zone, remove the card from the hand.
             let missingCard =
               gameScene.playerHand.evdenceCards.lastIndexOf(gameObject);
             gameScene.playerHand.evdenceCards[missingCard] = null;
-          } else {
+            socket.emit('EvidenceDropped', JSON.stringify(gameObject));
+          }
+
+          // if droped in the Blame pile
+          else {
+            // if evidence card was dropped, send it back to the hand
             if (gameObject.data.list.cardType !== 'blame') {
               gameObject.scale = 0.05;
               gameObject.x = gameObject.input.dragStartX;
               gameObject.y = gameObject.input.dragStartY;
               return;
             }
+
+            // else remove the card from the hand
             let missingCard =
               gameScene.playerHand.blameCards.lastIndexOf(gameObject);
             gameScene.playerHand.blameCards[missingCard] = null;
