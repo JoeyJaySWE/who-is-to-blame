9aa048861d2a2def4eee75847abcdc6265931774
JoeyJaySWE <70512017+JoeyJaySWE@users.noreply.github.com>
2021-05-25 22:21:18 +0000

    Deploying to deploy-server from @ JoeyJaySWE/who-is-to-blame@a8316d98d0ed9de3aa087d13fb0442ebc8e91fea 🚀

--- socket.js
+++ socket.js
@@ -9,7 +9,6 @@ module.exports = {
       pingTimeout: 60000,
       cors: {
         origin: 'https://who-is-to-blame.netlify.app/',
-        allowedHeaders: ['79kP9GCzrBP93meWdDyTunQt.v'],
       },
     });
     let players = {
